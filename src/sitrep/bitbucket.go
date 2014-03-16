663544ef69735d32d94749244932b7f5bb2a9f01
Iain Cambridge <github@icambridge.me>
2014-03-16 18:19:26 +0000

    Move bitbucket structs to gobucket

--- src/sitrep/bitbucket.go
+++ src/sitrep/bitbucket.go
@@ -4,13 +4,14 @@ import (
   "log"
   "encoding/json"
   "net/http"
+  "github.com/icambridge/gobucket"
 )
 
 func BitbucketHook(w http.ResponseWriter, r *http.Request) {
   log.Println("=== START OF BITBUCKET ===")
   r.ParseForm()
   log.Println(r.Form["payload"][0])
-  var h Hook
+  var h gobucket.Hook
   err := json.Unmarshal([]byte(r.Form["payload"][0]), &h)
   if err != nil {
     log.Println(err)
