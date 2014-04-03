bc8e380e61afd43c0cdab8fd2c3c5c830493fdc4
Iain Cambridge <github@icambridge.me>
2014-04-03 20:38:26 +0100

    Controller changes

--- src/framework/controller.go
+++ src/framework/controller.go
@@ -1,6 +1,23 @@
 package framework
 
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
 type Controller struct {
+
+	request http.Request
+
+	response http.Response
 
 }
 
+func (c *Controller) displayView(view string, data map[string]interface{}) {
+	c.response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t :=  template.Must(template.New("*").ParseFiles("templates/base.html", fmt.Sprintf("templates/%s.html", view)))
+	data["Section"] = template
+	t.ExecuteTemplate(w, "base", data)
+
+}
