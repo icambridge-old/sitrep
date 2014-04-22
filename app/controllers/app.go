bd20fa0aa67993427e96c6deea5b27c44922e8f0
Iain Cambridge <github@icambridge.me>
2014-04-22 22:25:14 +0100

    Add list all pull request ajax endpoint.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,8 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/revel/revel"
+	"github.com/icambridge/gobucket"
 	"sitrep/app/services"
+	"sitrep/app/converters"
+	"sitrep/app/entities"
+	"strings"
 )
 
 type App struct {
@@ -20,4 +25,34 @@ func (c App) Index() revel.Result {
 	jobs := jobsValue.Jobs
 
 	return c.Render(jobs)
+}
+
+func (c App) All() revel.Result {
+
+	pullRequests := []*gobucket.PullRequest{}
+	bitbucket := services.GetBitbucket()
+	bitbucketOwner := revel.Config.StringDefault("bitbucket.owner", "")
+
+	repos, err := bitbucket.Repositories.GetAll(bitbucketOwner)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, repo := range repos {
+		parts := strings.Split(repo.FullName, "/")
+		newRequests, err := bitbucket.PullRequests.GetAll(bitbucketOwner, parts[1])
+		if err != nil {
+			panic(err)
+		}
+		pullRequests = append(pullRequests, newRequests...)
+	}
+	convertedPrs := converters.GobucketToSitRepMultiPrs(pullRequests)
+
+	entity := entities.RepoInfo{PullRequests: convertedPrs}
+
+	jsonData, _ := json.Marshal(entity)
+	json := string(jsonData)
+	c.Request.Format = "json"
+	return c.Render(json)
 }
